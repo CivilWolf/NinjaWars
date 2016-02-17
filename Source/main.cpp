11df0a93057a0d703845de1225d167a57b24e53b
CivilWolf <civilwo1f254@gmail.com>
2016-02-17 14:02:48 -0600

    Main Added

---
+++ Source/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 17, 2016
+ *      Author: JonathanOlvera
+ */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+
+
+
+
